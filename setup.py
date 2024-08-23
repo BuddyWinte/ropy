b0a9e75c105df6f1f21ec0c4c20ff04cc1e30bdd
BuddyWinte <BuddyWinte@gmail.com>
2024-08-22 20:02:04 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ropy',
+    version='0.0.0',
+    packages=find_packages(),
+    description='A simple Python package',
+    long_description='This is a simple package with a single function.',
+    long_description_content_type='text/plain',
+    url='https://github.com/BuddyWinte/ropy',
+    author='BuddyWinte',
+    author_email='BuddyWinte@gmail.com',
+    license='MIT',
+)
